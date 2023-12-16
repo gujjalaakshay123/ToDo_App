a4c728104313d1e30acd7a697c5fec9556cbaf90
gujjalaakshay123 <gujjalaakshay123@gmail.com>
2023-12-16 10:29:15 -0500

    final project submission

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -7,9 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
         password:  document.getElementById('passWord').value
         }
 
-        console.log('Username:', details.username);
-        console.log('Password:', details.password);
-        console.log(details)
+        // console.log('Username:', details.username);
+        // console.log('Password:', details.password);
+        // console.log(details)
+        fetch('http://127.0.0.1:3000/users/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(details)
+        })
+        .then(data => data.json())
+        .then(userData => {
+            if (!userData.message){
+                console.log(userData)
+                localStorage.setItem("userData", JSON.stringify(userData))
+                window.location.href='home.html'
+            }
+            // else{
+            //     let errorMessage = document.querySelector("loginForm .error")
+            //     errorMessage.innerText = userData.message;
+            //     document.getElementById('userName').value = ''
+            //     document.getElementById('passWord').value = ''
+            //     console.log(userData)
+            // }
+        })
+        .catch(error =>{
+            console.log(error)
+            let errorMessage = document.querySelector('#loginForm .error')
+            errorMessage.innerHTML = `<p>${error.message}</p>`
+            document.getElementById('userName').value = ''
+            document.getElementById('passWord').value = ''
+        })
     }
 
     var loginForm = document.getElementById('loginForm');
